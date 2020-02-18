68ce0662048e5faca6dbfad449de672fb0bad1b6
Stephan van Rooij <github@svrooij.nl>
2020-02-18 16:20:21 +0100

    chore: Package updates

--- examples/alarms.js
+++ examples/alarms.js
@@ -1,17 +1,17 @@
 const SonosDevice = require('../lib').SonosDevice
 
 const sonos = new SonosDevice(process.env.SONOS_HOST || '192.168.96.56')
-sonos.AlarmList()
+sonos.AlarmClockService.ListAndParseAlarms()
   .then(alarms => {
     alarms.forEach(alarm => {
-      console.log('Alarm %d  %s is %s', alarm.ID, alarm.StartLocalTime, alarm.Enabled === 1 ? 'enabled' : 'disabled')
+      console.log('Alarm %d  %s is %s', alarm.ID, alarm.StartLocalTime, alarm.Enabled ? 'enabled' : 'disabled')
     })
   })
   .catch(err => {
     console.log(err)
   })
 
-// sonos.AlarmPatch({ ID: 1711, Enabled: false })
+// sonos.AlarmClockService.PatchAlarm({ ID: 1711, Enabled: false })
 //   .then(result => console.log(result))
 //   .catch(err => {
 //     console.error(err)
