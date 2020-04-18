30e61fe0d9f138a22df5cefcb61cf3f34652abbb
Stephan van Rooij <github@svrooij.nl>
2020-04-18 22:07:03 +0200

    chore: Linted tests and more Jest

--- tests/metadata-helper.test.ts
+++ tests/metadata-helper.test.ts
@@ -1,20 +1,20 @@
-import { MetadataHelper } from '../src/helpers/metadata-helper'
-import { expect, assert}  from 'chai'
-import 'mocha';
+import MetadataHelper from '../src/helpers/metadata-helper';
+import { expect, assert}  from 'chai';
+
 const GenerateMetadata = require('./legacy-helpers').GenerateMetadata
 
 const spotifyTrack = 'spotify:track:6sYJuVcEu4gFHmeTLdHzRz'
 
-describe('MetadataHelper', function () {
-  it('Guess metadata for Spotify artist top tracks', function() {
+describe('MetadataHelper', () => {
+  it('Guess metadata for Spotify artist top tracks', () => {
     const track = MetadataHelper.GuessTrack('spotify:artistTopTracks:72qVrKXRp9GeFQOesj0Pmv')
     expect(track).to.be.an('object')
   })
-  it('Guess metadata for Spotify track', function() {
+  it('Guess metadata for Spotify track', () => {
     const track = MetadataHelper.GuessTrack(spotifyTrack)
     expect(track).to.be.an('object')
   })
-  it('Produces same metadata as legacy for Spotify track', function() {
+  it('Produces same metadata as legacy for Spotify track', () => {
     const track = MetadataHelper.GuessTrack(spotifyTrack)
     expect(track).to.be.an('object')
     const legacyMetadataObject = GenerateMetadata(spotifyTrack, '', '2311')
--- tests/sonos-device.test.ts
+++ tests/sonos-device.test.ts
@@ -1,20 +1,18 @@
-import { SonosDevice } from '../src/sonos-device'
+import SonosDevice from '../src/sonos-device'
 import { expect }  from 'chai'
-import 'mocha';
 
-(process.env.SONOS_HOST ? describe : describe.skip)('SonosDevice - local', function () {
-  it('Loads alarms correctly', async function() {
+(process.env.SONOS_HOST ? describe : describe.skip)('SonosDevice - local', () => {
+  it('Loads alarms correctly', async () => {
     const device = new SonosDevice(process.env.SONOS_HOST || '')
     const alarms = await device.AlarmList()
     expect(alarms).to.be.an('array');
     expect(alarms).to.have.length.greaterThan(1)
   })
 
-  it('Joins a device by name', async function() {
+  it('Joins a device by name', async () => {
     const device = new SonosDevice(process.env.SONOS_HOST || '')
     const result = await device.JoinGroup('Slaapkamer')
     await device.AVTransportService.BecomeCoordinatorOfStandaloneGroup();
     expect(result).to.be.eq(true);
   })
-
 })
