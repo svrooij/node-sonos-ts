cac4052bdfd799890e0b0501baf5cb1ea13256de
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2021-05-06 18:31:39 +0200

    fix: Return empty array when input is undefined
    Fixed #136

--- src/helpers/array-helper.ts
+++ src/helpers/array-helper.ts
@@ -1,5 +1,8 @@
 export default class ArrayHelper {
   static ForceArray<TResponse>(input: TResponse | Array<TResponse>): Array<TResponse> {
+    if (input === undefined) {
+      return [];
+    }
     return Array.isArray(input) ? input : [input];
   }
 }
