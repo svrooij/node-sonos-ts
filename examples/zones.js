2701914377b540bab4a5cea2ec45413c00af10d3
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2021-07-05 22:30:28 +0200

    fix: Added Zone group ID and other props
    Every group now exposes it group id and each member
    now has   TVConfigurationError, HdmiCecAvailable,
    HasConfiguredSSID, WifiEnabled.
    
    Fixed: #132

--- examples/zones.js
+++ examples/zones.js
@@ -2,6 +2,10 @@ const SonosDevice = require('../lib').SonosDevice
 
 const sonos = new SonosDevice(process.env.SONOS_HOST || '192.168.96.56')
 
-sonos.JoinGroup('keuken')
-  .then(console.log)
-  .catch(console.error)
+// sonos.JoinGroup('keuken')
+//   .then(console.log)
+//   .catch(console.error)
+
+sonos.ZoneGroupTopologyService.Events.on('serviceEvent', (data) => {
+  console.log(data);
+});
