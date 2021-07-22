a6346f2d10be97da1637e3a6d7377046558ec106
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2021-07-22 16:46:35 +0200

    fix: Strong typed events (#140)
    * fix: Switched typed emitter library
    
    * fix: Changes to notifications

--- src/models/sonos-state.ts
+++ src/models/sonos-state.ts
@@ -2,8 +2,9 @@ import { GetMediaInfoResponse, GetPositionInfoResponse } from '../services';
 import { TransportState } from './transport-state';
 
 export interface SonosState {
-  transportState: TransportState;
   mediaInfo: GetMediaInfoResponse;
+  muted: boolean;
   positionInfo: GetPositionInfoResponse;
+  transportState: TransportState;
   volume: number;
 }
