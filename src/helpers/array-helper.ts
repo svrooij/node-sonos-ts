65d61a869c9b38629693dcc006a5f703c997a115
Stephan van Rooij <1292510+svrooij@users.noreply.github.com>
2023-01-20 10:33:34 +0100

    fix: Added docs on Array Helper

--- src/helpers/array-helper.ts
+++ src/helpers/array-helper.ts
@@ -1,3 +1,6 @@
+/**
+ * Array helper is to convert objects to single item arrays.
+ */
 export default class ArrayHelper {
   static ForceArray<TResponse>(input: TResponse | Array<TResponse>): Array<TResponse> {
     if (input === undefined) {
